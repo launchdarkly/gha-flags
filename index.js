5d450dceb0e0b7993b9949cdc1182029675aaa50
Clifford Tawiah <ctawiah@launchdarkly.com>
2022-06-13 12:47:46 -0500

    Setup ld client for flag evaluations

---
+++ index.js
@@ -0,0 +1,12 @@
+import { evaluateFlag } from "./client";
+
+const main = async () => {
+  // parse and validate args
+
+  // evaluate flags
+  // const result = await evaluateFlag('sdkKey', 'flagKey', 'defaultValue', {})
+
+  // set output
+};
+
+main();
