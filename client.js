ff9830de331451eb08907847ca59ed849a536a72
Jaz White <jwhite@launchdarkly.com>
2022-06-13 15:54:47 -0400

    add linting configuration (#5)
    * add pre-commit configuration
    * Add eslint and prettier

--- client.js
+++ client.js
@@ -10,13 +10,13 @@ const getClient = async (sdkKey) => {
 
   ldClient = LaunchDarkly.init(sdkKey);
   return await ldClient.waitForInitialization();
-}
+};
 
 export const evaluateFlag = async (sdkKey, flagKey, defaultValue, customProps = {}) => {
   const client = getClient(sdkKey);
   const context = {
     key: 'ld-github-action-flags',
     custom: customProps,
-  }
+  };
   return client.variation(flagKey, context, defaultValue);
-}
+};
--- configuration.js
+++ configuration.js
@@ -16,4 +16,4 @@ export const validate = (args) => {
   }
 
   return errors;
-}
+};
