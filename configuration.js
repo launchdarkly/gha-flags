57ca2abed708be212f984477b2b76b398316b28f
Jaz White <jwhite@launchdarkly.com>
2022-06-13 14:45:28 -0400

    [sc-156700] validate input args and log issues (#4)

---
+++ configuration.js
@@ -0,0 +1,19 @@
+import core from '@actions/core';
+
+export const validate = (args) => {
+  const errors = [];
+  if (!args.sdkKey) {
+    core.error('SDK key is required');
+    errors.push('sdk-key');
+  } else if (!args.sdkKey.startsWith('sdk-')) {
+    core.error('SDK key must start with "sdk-"');
+    errors.push('sdk-key');
+  }
+
+  if (!args.flagKeys) {
+    core.error('At least one flag key is required');
+    errors.push('flag-keys');
+  }
+
+  return errors;
+}
