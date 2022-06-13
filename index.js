0f897c8615d7e1f923906e384cf226832af2f39b
Jaz White <jwhite@launchdarkly.com>
2022-06-13 14:35:51 -0400

    validate input args and log issues

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import { evaluateFlag } from "./client";
+import { validateArgs } from "./input";
+import core from '@actions/core';
 
 const main = async () => {
   // parse and validate args
+  core.startGroup('Validating arguments');
+  const sdkKey = core.getInput('sdk-key');
+  core.setSecret(sdkKey);
+  const flagKeys = core.getMultilineInput('flag-keys');
+  const validationErrors = validateArgs({ sdkKey, flagKeys });
+
+  if (validationErrors) {
+    core.setFailed('Invalid arguments: ' + validationErrors.join(', '));
+    return;
+  }
+  core.endGroup();
 
   // evaluate flags
   // const result = await evaluateFlag('sdkKey', 'flagKey', 'defaultValue', {})
