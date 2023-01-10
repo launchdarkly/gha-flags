7581becb755750de168334c74972de064ff51906
Daniel OBrien <dobrien@launchdarkly.com>
2023-01-09 19:20:26 -0500

    fix filter

--- action.js
+++ action.js
@@ -69,7 +69,7 @@ export const run = async () => {
     ldCtx = {
       GithubCustomAttributes: {
         key: userKey ? userKey : Date.now(),
-        ...createContext(envLDFilters, 'LD_'),
+        ...createContext(envLDFilters),
       },
     };
   }
@@ -120,16 +120,17 @@ export const run = async () => {
   return;
 };
 
-function createContext(envFilters, ctxKey) {
+function createContext(envFilters, ignoreKey = '') {
   const ctx = {};
   Object.keys(process.env)
     .filter(function (key) {
-      return process.env[key] != '';
+      return process.env[key] == '';
     })
     .filter(function (key) {
-      if (key === ctxKey) {
-        return;
+      if (key === ignoreKey) {
+        return false;
       }
+      return true;
     })
     .forEach(function (key) {
       envFilters.forEach(function (p) {
